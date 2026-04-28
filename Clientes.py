9d99a08593f3e9dd6de2e568e91441705290adc7
Leandro <ggemes32@gmail.com>
2026-04-28 12:28:35 -0500

    Implementación de encapsulamiento y validación de atributos en la clase cliente - Jesus Leandro Garcia Osorio

--- Clientes.py
+++ Clientes.py
@@ -19,5 +19,42 @@ class cliente(entidad):
     
     def mostrar_detalle(self):
         print(f"Nombre: {self._nombre}, |Edad: {self._edad}, |Estado: {self._estado}")
+
+    # nombre
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, valor):
+        if valor.strip() == "":
+            raise ValueError("El nombre no puede estar vacio")
+        self._nombre = valor
+    
+    # edad
+    @property
+    def edad(self):
+        return self._edad
+    
+    @edad.setter
+    def edad(self, valor):
+        if valor <= 0:
+            raise ValueError("La edad debe ser mayor a 0")
+        self._edad = valor
+        
+    # estado
+    @property
+    def estado(self):
+        return self._estado
+    
+    @estado.setter
+    def estado(self, valor):
+        if valor.lower() in ["activo", "inactivo"]:
+            self._estado = valor.lower()
+        else:
+            raise ValueError("Estado no valido")
+        
+        
+    
         
      
